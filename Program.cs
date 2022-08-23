4a4408501ca2041b481b0816afa270a1a152d8c0
ina1ecSc <f.inaie4191@gmail.com>
2022-08-23 08:39:00 -0300

    Primeiro commmit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MeuApp
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+
+            var sabao = 21;
+            if (sabao == 20)
+            {
+            var mund = "Olá mundo";
+            Console.WriteLine(mund);
+            }
+            else if (sabao == 21)
+            {
+                var mundo = "Olá, amigo.";
+                Console.WriteLine(mundo);
+            }
+        }
+    }
+}
